ca88b88eecaf48772d254e84c4127a3b9a3c05c3
Mateus <mateus.augusto9@outlook.com>
2023-05-07 17:47:02 -0300

    Passo8

--- src/Caixa.java
+++ src/Caixa.java
@@ -2,58 +2,167 @@ import java.util.Scanner;
 
 public class Caixa {
   private ContaBancaria c1;
+  private ContaBancaria c2;
+  private Cliente cliente;
+  private Scanner entrada=new Scanner(System.in);
 
   public Caixa(){
     this.c1=null;
+    this.c2=null;
+    this.cliente=null;
   }
 
   public void criarConta(){
-    this.c1=new ContaBancaria();
+    System.out.println("Digite seu nome: ");
+    String nome=entrada.nextLine();
+    System.out.println("Digite seu CPF: ");
+    String cpf=entrada.nextLine();
+
+    for(int i=0; i<2; i++){
+      if(i==1){
+        System.out.println("Digite o tipo da conta: \n" +
+        "Digite 1(Conta sem informar o saldo)" +
+        "ou Digite 2(Conta com saldo)"
+        );
+        String tipoConta=entrada.nextLine();
+        System.out.println("Digite o limite da conta: ");
+        double limite=Double.parseDouble(entrada.nextLine());
+  
+        cliente=new Cliente(nome, cpf);
+  
+        if(tipoConta.equals("1")){
+          this.c1=new ContaBancaria(cliente, limite);
+          System.out.println("Numero da conta: " + this.c1.getNroConta());
+        }else if(tipoConta.equals("2")){
+          System.out.println("Digite o saldo da conta: ");
+          int saldo=Integer.parseInt(entrada.nextLine());
+          this.c1=new ContaBancaria(cliente, limite, saldo);
+          System.out.println("Numero da conta: " + this.c1.getNroConta());
+        }else{
+          System.out.println("Tipo de conta invalido!");
+        }
+      }else{
+        System.out.println("Digite o tipo da conta: \n" +
+        "Digite 1(Conta sem informar o saldo)" +
+        "ou Digite 2(Conta com saldo)"
+        );
+        String tipoConta=entrada.nextLine();
+        System.out.println("Digite o limite da conta: ");
+        double limite=Double.parseDouble(entrada.nextLine());
+
+        cliente=new Cliente(nome, cpf);
+
+        if(tipoConta.equals("1")){
+          this.c2=new ContaBancaria(cliente, limite);
+          System.out.println("Numero da conta: " + this.c2.getNroConta());
+        }else if(tipoConta.equals("2")){
+          System.out.println("Digite o saldo da conta: ");
+          int saldo=Integer.parseInt(entrada.nextLine());
+          this.c2=new ContaBancaria(cliente, limite, saldo);
+          System.out.println("Numero da conta: " + this.c2.getNroConta());
+        }else{
+          System.out.println("Tipo de conta invalido!");
+        }
+      }
+    }
   }
 
-  public double consultarSaldo(){
-    return this.c1.getSaldo();
+  public void getSaldo(int nroConta){
+    if(nroConta==this.c1.getNroConta()){
+      System.out.println("Nome: " + this.cliente.getNome() + ", Saldo: " + this.c1.getSaldo());
+    }else if(nroConta==this.c2.getNroConta()){
+      System.out.println("Nome: " + this.cliente.getNome() + ", Saldo: " + this.c2.getSaldo());
+    }else{
+      System.out.println("Numero de conta invalida!");
+    }
   }
 
-  public void depositar(double valor){
-    this.c1.depositar(valor);
+  public void depositar(int nroConta, double valor){
+    if(nroConta==this.c1.getNroConta()){
+      this.c1.depositar(valor);
+    }else if(nroConta==this.c2.getNroConta()){
+      this.c2.depositar(valor);
+    }else{
+      System.out.println("Numero de conta invalida!");
+    }
   }
 
-  public void sacar(double valor){
-    if(this.c1.sacar(valor)==true){
-      this.c1.sacar(valor);
+  public void sacar(int nroConta, double valor){
+    if(this.c1.sacar(valor)==true && nroConta==this.c1.getNroConta()){
+      System.out.println("Saque realizado com sucesso!");
+    }else if(this.c2.sacar(valor)==true && nroConta==this.c2.getNroConta()){
+      System.out.println("Saque realizado com sucesso!");
+    }else if(nroConta!=this.c1.getNroConta() && nroConta!=this.c2.getNroConta()){
+      System.out.println("Numero de conta invalida!");
     }else{
       System.out.println("Limite de valor da conta atingido!");
     }
   }
 
+  public void transferir(int nroContaOrigem, int nroContaDestino){
+    System.out.println("Digite o valor a ser transferido: ");
+    int valor=Integer.parseInt(entrada.nextLine());
+    
+    if(this.c1.getNroConta()==nroContaOrigem && this.c2.getNroConta()==nroContaDestino && this.c1.transferir(valor, c2)==true){
+      System.out.println("Transferencia realizada com sucesso!");
+    }else if(this.c2.getNroConta()==nroContaOrigem && this.c1.getNroConta()==nroContaDestino && this.c2.transferir(valor, c1)==true){
+      System.out.println("Transferencia realizada com sucesso!");
+    }else if(this.c1.getNroConta()!=nroContaOrigem && this.c2.getNroConta()!=nroContaOrigem){
+      System.out.println("Transferencia invalida! Numero Origem inexistente.");
+    }else if(this.c1.getNroConta()!=nroContaDestino && this.c2.getNroConta()!=nroContaDestino){
+      System.out.println("Transferencia invalida! Numero Destino inexistente.");
+    }else{
+      System.out.println("Transferencia invalida! Valor de transferencia acima do limite!");
+    }
+  }
+
   public void menu(){
     System.out.println(
-    "1. Criar Conta /n"+
-    "2. Consultar Saldo /n"+
-    "3. Depositar /n"+
-    "4. Sacar /n"+
-    "5. Sair"
+    "1. Criar Conta \n"+
+    "2. Consultar Saldo \n"+
+    "3. Depositar \n"+
+    "4. Sacar \n"+
+    "5. Transferir \n"+
+    "6. Sair"
     );
+  }
 
-    Scanner entrada=new Scanner(System.in);
+  public void executar(){
+    menu();
     String opc=entrada.nextLine();
     int valor;
+    int nroConta;
 
-    while(!opc.equals("5")){
+    while(!opc.equals("6")){
       if(opc.equals("1")){
         criarConta();
       }else if(opc.equals("2")){
-        consultarSaldo();
+        System.out.println("Informe o numero da conta: ");
+        nroConta=Integer.parseInt(entrada.nextLine());
+        getSaldo(nroConta);
       }else if(opc.equals("3")){
+        System.out.println("Informe o numero da conta: ");
+        nroConta=Integer.parseInt(entrada.nextLine());
+        System.out.println("Digite o valor que deseja depositar: ");
         valor=Integer.parseInt(entrada.nextLine());
-        depositar(valor);
+        depositar(nroConta, valor);
       }else if(opc.equals("4")){
+        System.out.println("Informe o numero da conta: ");
+        nroConta=Integer.parseInt(entrada.nextLine());
+        System.out.println("Digite o valor que deseja sacar: ");
         valor=Integer.parseInt(entrada.nextLine());
-        sacar(valor);
+        sacar(nroConta, valor);
+      }else if(opc.equals("5")){
+        System.out.println("Digite o numero da conta Origem: ");
+        int nroContaOrigem=Integer.parseInt(entrada.nextLine());
+        System.out.println("Digite o numero da conta Destino: ");
+        int nroContaDestino=Integer.parseInt(entrada.nextLine());
+        transferir(nroContaOrigem, nroContaDestino);
       }else{
         System.out.println("Opcao invalida!");
       }
+      menu();
+      opc=entrada.nextLine();
     }
     entrada.close();
   }
--- src/ContaBancaria.java
+++ src/ContaBancaria.java
@@ -1,10 +1,22 @@
 public class ContaBancaria {
   private double saldo;
   private double limite;
+  private Cliente c1;
+  private int nroConta;
+  private static int ultimaConta=100;
 
-  public ContaBancaria(){
+  public ContaBancaria(Cliente c1, double limite){
+    this.c1=c1;
+    this.limite=-limite;
     this.saldo=0;
-    this.limite=500;
+    this.nroConta=ultimaConta++;
+  }
+
+  public ContaBancaria(Cliente c1, double limite, double saldo){
+    this.c1=c1;
+    this.limite=-limite;
+    this.saldo=saldo;
+    this.nroConta=ultimaConta++;
   }
 
   public double getSaldo(){
@@ -12,7 +24,7 @@ public class ContaBancaria {
   }
 
   public boolean sacar(double valor){
-    if(valor<=this.limite){
+    if(this.saldo-valor>=this.limite){
       this.saldo-=valor;
       return true;
     }
@@ -21,5 +33,17 @@ public class ContaBancaria {
 
   public void depositar(double valor){
     this.saldo+=valor;
+  }
+
+  public int getNroConta(){
+    return nroConta;
+  }
+
+  public boolean transferir(int valor, ContaBancaria contaDestino){
+    if(this.sacar(valor)==true){
+      contaDestino.depositar(valor);
+      return true;
+    }
+    return false;
   }
 }
